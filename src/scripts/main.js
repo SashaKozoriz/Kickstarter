950d9b2facc3dffe61bf281183d1f3716f8e8e0c
Sasha <sashakozoriz2012@gmail.com>
2024-12-23 19:12:12 +0200

    Add new styles for page, section, benefits, and product blocks; update reset and header styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const enButton = document.getElementById('en');
+const uaButton = document.getElementById('ua');
+
+enButton.addEventListener('click', () => {
+  uaButton.classList.add('nav__link__lang--not-active');
+  enButton.classList.remove('nav__link__lang--not-active');
+});
+
+uaButton.addEventListener('click', () => {
+  enButton.classList.add('nav__link__lang--not-active');
+  uaButton.classList.remove('nav__link__lang--not-active');
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
